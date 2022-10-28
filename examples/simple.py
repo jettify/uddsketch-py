f913d13df3671675084e219677ecbf3a29ed55fc
Nikolay Novik <nickolainovik@gmail.com>
2022-10-27 22:50:09 -0400

    Update simple example and README. (#79)

--- examples/simple.py
+++ examples/simple.py
@@ -2,7 +2,16 @@ from uddsketch import UDDSketch
 
 hist = UDDSketch(initial_error=0.01)
 
+# Populate structure with dummy data
 for i in range(0, 100):
     hist.add(0.1 * i)
-q = hist.quantile(0.5)
-print("quantie: {}".format(q))
+
+# Estimate p95 percentile
+q = hist.quantile(0.95)
+print(f"quantie: {q}")
+# quantie: 9.487973051696695
+
+# Estimate median
+m = hist.median()
+print(f"median: {m}")
+# median: 4.903763642930295
